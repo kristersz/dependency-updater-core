c77dc33acdd4cb57c44e0f03f805d9bcc7fbb6e5
Kristers Zīmecs <kristers.zimecs@if.lv>
2019-08-21 10:54:13 +0300

    NuGetClient test

---
+++ DependencyUpdaterCore/DependencyUpdaterCore.Tests/Clients/NuGetClientTest.cs
@@ -0,0 +1,18 @@
+﻿using DependencyUpdaterCore.Clients.NuGet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace DependencyUpdaterCore.Tests.Clients
+{
+    [TestClass]
+    public class NuGetClientTest
+    {
+        [TestMethod]
+        public async Task SearchUpdates_SamplePackage_ReturnsVersion()
+        {
+            var client = new NuGetClient();
+
+            await client.SearchUpdates();
+        }
+    }
+}
--- DependencyUpdaterCore/DependencyUpdaterCore/Clients/NuGet/NuGetClient.cs
+++ DependencyUpdaterCore/DependencyUpdaterCore/Clients/NuGet/NuGetClient.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace DependencyUpdaterCore.Clients.NuGet
 {
     public class NuGetClient : INuGetClient
     {
-        
+        public async Task SearchUpdates()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var packageId = "If.ClaimsForms.Bot.Models";
+
+                var requestUri = $"http://cloudgallery.ifext.biz/nuget/If-Nuget/{packageId}/index.json";
+                var response = await httpClient.GetAsync(requestUri);
+            }
+        }
     }
 }
