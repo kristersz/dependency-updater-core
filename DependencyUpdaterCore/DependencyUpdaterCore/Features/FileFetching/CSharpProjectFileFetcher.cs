f230273bca58e104b0e482d73cec4d643bbc37cb
Kristers Zīmecs <kristers.zimecs@if.lv>
2019-08-21 17:52:23 +0300

    File fetcher

---
+++ DependencyUpdaterCore/DependencyUpdaterCore/Features/FileFetching/CSharpProjectFileFetcher.cs
@@ -0,0 +1,22 @@
+﻿using DependencyUpdaterCore.Clients.AzureDevOps;
+using DependencyUpdaterCore.Models.AzureDevOpsClient;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DependencyUpdaterCore.Features.FileFetching
+{
+    public class CSharpProjectFileFetcher : ICSharpProjectFileFetcher
+    {
+        private readonly IAzureDevOpsClient _azureDevOpsClient;
+
+        public CSharpProjectFileFetcher(IAzureDevOpsClient azureDevOpsClient)
+        {
+            _azureDevOpsClient = azureDevOpsClient;
+        }
+
+        public async Task<IList<ICsProjResponse>> GetProjectDependencyFileAsync()
+        {
+            return await _azureDevOpsClient.GetProjectDependencyFileAsync();
+        }
+    }
+}
---
+++ DependencyUpdaterCore/DependencyUpdaterCore/Features/FileFetching/ICSharpProjectFileFetcher.cs
@@ -0,0 +1,11 @@
+﻿using DependencyUpdaterCore.Models.AzureDevOpsClient;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DependencyUpdaterCore.Features.FileFetching
+{
+    public interface ICSharpProjectFileFetcher
+    {
+        Task<IList<ICsProjResponse>> GetProjectDependencyFileAsync();
+    }
+}
