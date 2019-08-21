8517b9b7bb17e03831fb74870dc65bfbe104ed59
Kristers Zīmecs <kristers.zimecs@if.lv>
2019-08-21 10:18:21 +0300

    NuGetClient class

---
+++ DependencyUpdaterCore/DependencyUpdaterCore/Clients/NuGet/INuGetClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyUpdaterCore.Clients.NuGet
+{
+    public interface INuGetClient
+    {
+    }
+}
---
+++ DependencyUpdaterCore/DependencyUpdaterCore/Clients/NuGet/NuGetClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyUpdaterCore.Clients.NuGet
+{
+    public class NuGetClient : INuGetClient
+    {
+        
+    }
+}
