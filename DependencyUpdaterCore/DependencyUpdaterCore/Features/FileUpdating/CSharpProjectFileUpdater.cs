b8cf51061c096456efa4bdcc6360756b0749b3b7
Kristers Zīmecs <kristers.zimecs@if.lv>
2019-08-21 15:23:02 +0300

    CSharpProjectFileUpdater

---
+++ DependencyUpdaterCore/DependencyUpdaterCore/Features/FileUpdating/CSharpProjectFileUpdater.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace DependencyUpdaterCore.Features.FileUpdating
+{
+    public class CSharpProjectFileUpdater
+    {
+        public void UpdateCsprojFile(XDocument projectFile, List<string> latestDependencies)
+        {
+
+        }
+    }
+}
