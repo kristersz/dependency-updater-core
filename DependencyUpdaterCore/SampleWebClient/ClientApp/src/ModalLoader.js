8d831ff3d5b012e43cc3369b12966cb131cec506
Gribusts, Toms <toms.gribusts@if.lv>
2019-08-22 12:58:54 +0300

    flexbox

--- DependencyUpdaterCore/SampleWebClient/ClientApp/src/ModalLoader.js
+++ DependencyUpdaterCore/SampleWebClient/ClientApp/src/ModalLoader.js
@@ -3,9 +3,8 @@ import Loader from 'react-loader-spinner'
 
 function ModalLoader() {
     return (
-        <div style={{ backgroundColor: "black", opacity: "0.5", position: 'absolute', width: "100%", height: "100%" }}>
+        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", backgroundColor: "black", opacity: "0.5", position: 'absolute', width: "100%", height: "100%" }}>
             <Loader
-                style={{ zIndex: 30, position: "absolute", top: "20%", left: "37%" }}
                 type="Watch"
                 color="#00BFFF"
                 height="300"
