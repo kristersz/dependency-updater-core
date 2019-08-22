608170df8ed6d31a5cfd1e2bbb2a69c13bc8a38a
Gribusts, Toms <toms.gribusts@if.lv>
2019-08-22 10:59:29 +0300

    ui

--- DependencyUpdaterCore/SampleWebClient/ClientApp/src/App.js
+++ DependencyUpdaterCore/SampleWebClient/ClientApp/src/App.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography variant="button" style={{ color: "black", marginBottom: 20 }}>
+          Package auto updater
+        </Typography>
+
+        <TextField
+          id="outlined-repository-input"
+          label="Repository"
+          variant="outlined"
+        />
+
+        <FormControlLabel
+          control={<Checkbox color="primary" />}
+          label="Include previews"
+          style={{ color: "black" }}
+        />
+        <FormControlLabel
+          control={<Checkbox color="primary" />}
+          label="Update major versions"
+          style={{ marginBottom: 30, color: "black" }}
+        />
+
+        <Button variant="contained" color="secondary" className="ButtonSpin">
+          Trigger
+        </Button>
       </header>
     </div>
   );
